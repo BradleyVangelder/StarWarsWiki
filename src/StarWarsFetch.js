221c1d07d89877e41c5807e7b09b9e3a3d879211
bradley vangelder <r0784535@student.thomasmore.be>
2020-12-02 09:21:22 +0100

    changes search function !BUG changeHandler is always one step behind

--- src/StarWarsFetch.js
+++ src/StarWarsFetch.js
@@ -4,7 +4,7 @@ function StarWarsFetch() {
 
     const [data, setData] = useState([])
     const [search, setSearch] = useState("")
-    const
+    const [dataShow, setDataShow] = useState([])
 
     useEffect(() => {
         async function fetchPeople() {
@@ -16,28 +16,45 @@ function StarWarsFetch() {
         fetchPeople()
     }, [])
 
-    function changeHandler(event) {
-        const {value} = event.target
-
-        setSearch(value)
-    }
-
-    function searchHandler() {
+    function searchFilter() {
+        const persons = []
         data.map(person => {
             if(person.name.includes(search)){
-
+                const prop = {
+                    name: person.name,
+                    height: person.height,
+                    hairColor: person.hair_color,
+                    birthYear: person.birth_year
+                }
+                persons.push(prop)
             }
         })
+
+        setDataShow(persons)
     }
+
+    function changeHandler(event) {
+        const {value} = event.target
+        setSearch(value)
+        searchFilter()
+    }
+
+    console.log(dataShow)
 
     return(
         <div>
             <h1>Star Wars Wiki</h1>
             <input type="text" name="search" value={search} onChange={changeHandler} />
-            <button
-            onClick={searchHandler}>
-                Search
-            </button>
+            {dataShow.map(person => {
+                return(
+                    <div>
+                        <h1>{person.name}</h1>
+                        <p>height: {person.height}</p>
+                        <p>haircolor: {person.hairColor}</p>
+                        <p>Birthyear: {person.birthYear}</p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
